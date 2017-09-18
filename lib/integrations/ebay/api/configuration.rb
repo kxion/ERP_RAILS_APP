b67f4a1b04bebd8240f62a74dc3f69bf22d40d28
vasanthakumar <vasanthakumar@aryvartdev.com>
2017-09-18 11:00:59 +0530

    eBay changes

--- lib/integrations/ebay/api/configuration.rb
+++ lib/integrations/ebay/api/configuration.rb
@@ -4,19 +4,19 @@ module Integrations
       module Configuration
 
         def dev_id
-          '6876a00f-97d4-4dc9-b2db-da216f2b84f4' if Rails.env.development? || Rails.env.staging?
+          '6876a00f-97d4-4dc9-b2db-da216f2b84f4' if Rails.env.development? || Rails.env.production? || Rails.env.staging?
         end
 
         def app_id
-          'CoreAuto-df73-4a63-a2fa-4ee210c6cbb6' if Rails.env.development? || Rails.env.staging?
+          'CoreAuto-df73-4a63-a2fa-4ee210c6cbb6' if Rails.env.development? || Rails.env.production? || Rails.env.staging?
         end
 
         def cert_id
-          '2fa035b8-8fa4-4e1a-996f-7aca47187407' if Rails.env.development? || Rails.env.staging?
+          '2fa035b8-8fa4-4e1a-996f-7aca47187407' if Rails.env.development? || Rails.env.production? || Rails.env.staging?
         end
 
         def ru_name
-          'Core_Automotive-CoreAuto-df73-4-huchtfv' if Rails.env.development? || Rails.env.staging?
+          'Core_Automotive-CoreAuto-df73-4-huchtfv' if Rails.env.development? || Rails.env.production? || Rails.env.staging?
         end
 
       end
